e77be917943eb0dba5785ddff134cecefbe4b640
Tharin <tharin.ch@chococrm.com>
2023-06-22 15:49:41 +0700

    add: router

---
+++ src/views/About.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    {{ ren() }}
+    <router-link :to="{ name: 'home' }">{{ "home" }}</router-link>
+  </div>
+</template>
+
+<script setup lang="ts">
+const ren = (): string => {
+  return "About";
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <router-link to="about">about</router-link>
+    {{ ren() }}</div>
+</template>
+
+<script setup lang="ts">
+const ren = (): string => {
+  return "Home";
+};
+</script>
+
+<style scoped></style>
